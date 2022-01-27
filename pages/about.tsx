68db17e73da8546dfd6dc083c834f3f5ddf1b94a
Truls Henrik <trulshj@stud.ntnu.no>
2022-01-27 19:59:08 +0100

    Fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,25 +1,6 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import homeStyles from "../styles/Home.module.css";
-import Footer from "../components/Footer";
-
 const About: NextPage = () => {
-  return (
-    <div className={homeStyles.container}>
-      <Head>
-        <title>About</title>
-        <meta
-          name="description"
-          content="A zine about Adam from SK8 being in love <33"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={homeStyles.main}>
-      </main>
-      <Footer></Footer>
-    </div>
-  );
+  return <div>Lmao gottem</div>;
 };
 
 export default About;
