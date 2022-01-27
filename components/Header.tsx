966ee2ba03cccf8dd7a24c20923be8c8bf3a97ab
Truls Henrik <trulshj@stud.ntnu.no>
2022-01-27 22:22:06 +0100

    Make title bigger

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
+import styles from "../styles/Header.module.css";
+
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>Adam in Love</h1>
     </header>
   );
