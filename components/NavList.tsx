f6886f5415b5e875c6e97173cd3d5ad5859c1b64
Truls Henrik <trulshj@stud.ntnu.no>
2022-01-27 21:36:45 +0100

    Make nav links actual links

--- components/NavList.tsx
+++ components/NavList.tsx
@@ -1,12 +1,19 @@
+import Link from "next/link";
 import LetterButton from "./LetterButton";
 import styles from "../styles/NavList.module.css";
 
 export default function NavList() {
   return (
     <nav className={styles.navList}>
-      <LetterButton letterColor="lightblue">Home</LetterButton>
-      <LetterButton letterColor="lightgreen">About</LetterButton>
-      <LetterButton>Zine</LetterButton>
+      <Link href='/' passHref>
+        <LetterButton letterColor='lightblue'>Home</LetterButton>
+      </Link>
+      <Link href='/about' passHref>
+        <LetterButton letterColor='lightgreen'>About</LetterButton>
+      </Link>
+      <Link href='/zine' passHref>
+        <LetterButton>Zine</LetterButton>
+      </Link>
     </nav>
   );
 }
