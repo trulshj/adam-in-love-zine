0d72b571a1b8f2655e3946ad096edd8e2f28ab25
Truls Henrik <trulshj@stud.ntnu.no>
2022-01-27 21:36:33 +0100

    Move letterbutton css file

--- components/LetterButton.tsx
+++ components/LetterButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./LetterButton.module.css";
+import styles from "../styles/LetterButton.module.css";
 
 interface LetterButtonProps {
   children: React.ReactNode;
