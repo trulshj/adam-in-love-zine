a41a202e608fee7b82ee0b1e46eaf2bf70704ada
Truls Henrik <trulshj@stud.ntnu.no>
2022-01-27 22:22:14 +0100

    Add chibi

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
 import FooterLink, { FooterLinkProps } from "./FooterLink";
+import Image from "next/image";
 
+import ainosuke from "../public/Ainosuke_chibi.png";
 import styles from "../styles/Footer.module.css";
 
 const footerProfiles: FooterLinkProps[] = [
@@ -10,6 +12,14 @@ const footerProfiles: FooterLinkProps[] = [
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <div className={styles.chibi}>
+        <Image
+          src={ainosuke}
+          alt='Chibi Ainosuke-sama'
+          width={200}
+          height={200}
+        />
+      </div>
       {footerProfiles.map((profile) => (
         <FooterLink
           key={profile.name + profile.verb}
